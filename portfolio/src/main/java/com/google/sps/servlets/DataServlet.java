f3ddbb35acca4664cb8cfe90d309f8e09942a6e9
Adam Weidman <adamfweidman@gmail.com>
2020-06-04 01:40:21 +0000

    remove unnecessary line

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,6 @@ import com.google.gson.Gson;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
-  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json;");
